37099a897a36f1e7826a1dc7204c7dab543e4c16
Claudia0729 <claudialin729@gmail.com>
2023-05-04 16:45:53 +0800

    add week12

---
+++ week12-1_printf_fprintf.cpp
@@ -0,0 +1,10 @@
+///printf_fprintf.cpp
+
+#include <stdio.h>
+int main()
+{
+    ///檔案指標 fout = fopen("檔名","模式");要開檔
+    FILE * fout = fopen("file.txt","w");
+    printf("Hello World");
+    fprintf(fout, "Hello World在檔案裡\n");
+}
